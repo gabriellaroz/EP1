631a74e0bfd8075a16cd20c5712d69a187a4fd93
Gabriella <gabriellaprozario@gmail.com>
2025-09-30 16:25:39 -0300

    classe data.cpp

---
+++ Data.cpp
@@ -0,0 +1,17 @@
+#include "Data.h"
+#include <iostream>
+
+using namespace std;
+
+Data::Data(int mes, int ano){
+    this->mes = mes;
+    this->ano = ano; 
+
+int get Data::getMes(){
+    return mes;
+}
+
+int Data::getAno(){
+    return ano;
+}
+}
